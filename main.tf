729462810a7149c449135abb3ab018751ea8ce15
root <root@ip-172-31-48-144.ec2.internal>
2019-04-24 14:54:44 -0400

    Changes to main.tf

--- main.tf
+++ main.tf
@@ -17,25 +17,6 @@ module "mysql_db" {
   source = "/home/centos/terraform/lamp/modules/datebases"
 }
 
-#locals {
-#ec2_sg_id = concat(aws_security_groups.ec2_sg.id, aws_security_groups.elb_sg.id)
-#}
-
-#resource "aws_db_instance" "mysql_db" { 
-#  allocated_storage = 20
-#  storage_type = "gp2"
-#  engine = "mysql"
-#  engine_version = "5.7"
-#  instance_class = "db.t2.micro"
-#  name = "testdb"
-#  username = "dbuser"
-#  password = "12FLoz355"
-#  parameter_group_name = "default.mysql5.7"
-#  identifier = "wordpress-db-qa"
-#  final_snapshot_identifier = "final-snapshot"
-#  skip_final_snapshot = true
-#}
-
 resource "aws_instance" "qa_app" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
